285f71792c06b614eb10674183d85fa111eaf308
yurii837 <gorogotskiy@gmail.com>
2022-05-16 07:42:37 +0300

    clone landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,55 @@
 'use strict';
+
+// disable reloading when submit form
+
+function stopDefAction(event) {
+  document.getElementById('form').reset();
+  event.preventDefault();
+}
+
+document.getElementById('form').addEventListener(
+  'submit', stopDefAction, false
+);
+
+// burger menu
+
+const nav = document.querySelector('.nav__main');
+const toggler = document.querySelector('.nav__icon--menu-opener');
+
+toggler.addEventListener('click', function() {
+  nav.classList.toggle('nav--open');
+  toggler.classList.toggle('nav--open');
+});
+
+// 100vw minus vertical scroll
+
+function setVw() {
+  const vw = document.documentElement.clientWidth / 100;
+
+  document.documentElement.style.setProperty('--vw', `${vw}px`);
+}
+
+setVw();
+window.addEventListener('resize', setVw);
+
+// slider
+
+const list = document.querySelector('.slider__list');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+let count = 1;
+
+function move(isNext) {
+  const moveValue = isNext ? 1 : -1;
+
+  list.style.setProperty('transform', `translateX(${moveValue * 300}px)`);
+}
+
+prevButton.addEventListener('click', function() {
+  count++;
+  move(false);
+});
+
+nextButton.addEventListener('click', function() {
+  move(true);
+});
